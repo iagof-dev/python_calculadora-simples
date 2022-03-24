3711da5d35fd5f8be45f5138bc4f37f3ef56a127
N3rdy <n3rdydzn@hotmail.com>
2022-03-24 20:56:55 -0300

    Commit v1.0

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+
+print("Calculadora Simples - Aprendendo Python")
+print("[1] Somar ")
+print("[2] Subtrair ")
+print("[3] Multiplicar ")
+print("https://n3rdydzn.xyz/ ")
+escolha = input("Digite o NÚMERO que desejas... ")
+
+if escolha == "1":
+    numero_1 = int(input("Digite o Primeiro Número "))
+    numero_2 = int(input("Digite o Segundo Número "))
+    print("Resultado foi:" , numero_1 + numero_2)
+if escolha == "2":
+    numero_1 = int(input("Digite o Primeiro Número "))
+    numero_2 = int(input("Digite o Segundo Número "))
+    print("Resultado foi:" , numero_1 - numero_2)
+if escolha == "3":
+    numero_1 = int(input("Digite o Primeiro Número "))
+    numero_2 = int(input("Digite o Segundo Número "))
+    print("Resultado foi:" , numero_1 * numero_2)
